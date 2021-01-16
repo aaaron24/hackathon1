d849ae6d2b781d46c964ba935783245ed72aa6ad
Aaron T <aronthakur@gmail.com>
2021-01-16 12:23:52 -0800

    initializing project

---
+++ endangered-species/src/main/java/com/hackathon/endangeredspecies/EndangeredSpeciesApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.endangeredspecies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndangeredSpeciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndangeredSpeciesApplication.class, args);
+	}
+
+}
